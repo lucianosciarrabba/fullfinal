2b91323eb4d866d98b0053592e7ce498dec71e80
lucianosciarrabba <168043176+lucianosciarrabba@users.noreply.github.com>
2024-11-25 13:20:03 -0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,22 @@
+// login.js
+document.getElementById('loginForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+  
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+  
+    // Aquí puedes poner tus credenciales fijas o verificar en Firebase.
+    // Vamos a usar un ejemplo simple con credenciales fijas:
+    const validUser = "tecnica";
+    const validPass = "Full.3456";
+  
+    if (username === validUser && password === validPass) {
+      // Guardamos un flag en el localStorage indicando que el usuario ha iniciado sesión.
+      localStorage.setItem('isLoggedIn', 'true');
+      // Redirigimos al usuario a la página principal.
+      window.location.href = "index.html";
+    } else {
+      alert("Usuario o contraseña incorrectos");
+    }
+  });
+  
